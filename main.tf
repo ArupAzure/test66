e95c3ccd75d286522d8a1f3ae2804dadc96f560f
ArupAzure <90151193+ArupAzure@users.noreply.github.com>
2025-01-12 22:07:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+# This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+  subscription_id = "cc9a57b5-2fb8-42dc-b0be-2a8c72b1f234"
+}
+
+# Create a resource group
+
+resource "random_pet" "rg_name" {
+  length    = 2
+  separator = "-"
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+
+}
+
